b655ce3dac3f98277e88fe719a96648fa698f9bd
Aaron Arinder <aaronarinder@gmail.com>
2025-07-15 10:55:05 -0400

    neovim: trouble with kotlin-lsp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -444,65 +444,41 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
+			-- manual registration of kotlin-lsp; not yet supported in :h lspconfig-all, so no automatic attachment
+			--local lspconfig = require("lspconfig")
+			--local configs = require("lspconfig.configs")
+			--if not configs.kotlin_lsp then
+			--	configs.kotlin_lsp = {
+			--		default_config = {
+			--			cmd = { vim.fn.stdpath("data") .. "/mason/bin/kotlin-lsp", "--stdio" },
+			--			filetypes = { "kotlin" },
+			--			root_dir = lspconfig.util.root_pattern("settings.gradle", "settings.gradle.kts", ".git"),
+			--		},
+			--	}
+			--end
+
 			--  Add any additional override configuration in the following tables. Available keys are:
 			--  - cmd (table): Override the default command used to start the server
 			--  - filetypes (table): Override the default list of associated filetypes for the server
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+			-- NOTE: See `:help lspconfig-all` for a list of all the pre-configured LSPs
+			-- NOTE: sometimes there's not support in lspconfig for an lsp (like kotlin-lsp), and the naming here might come apart from
+			-- the binary name; be careful with how they're added, relate it to the ensure_installed list below (for the actual binary name)
 			local servers = {
-				-- NOTE: See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--terraformls = {},
-				--nixd = {},
-				--kotlin_language_server = {},
-				--rust_analyzer = {},
-				--pylsp = {
-				--	settings = {
-				--		pylsp = {
-				--			plugins = {
-				--				pycodestyle = {
-				--					ignore = { "W391" },
-				--					maxLineLength = 100,
-				--				},
-				--			},
-				--		},
-				--	},
-				--},
-				--tsserver = {},
-				--lua_ls = {
-				--	-- cmd = {...},
-				--	-- filetypes = { ...},
-				--	-- capabilities = {},
-				--	settings = {
-				--		Lua = {
-				--			completion = {
-				--				callSnippet = "Replace",
-				--			},
-				--			-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-				--			-- diagnostics = { disable = { 'missing-fields' } },
-				--		},
-				--	},
-				--},
+				--kotlin_lsp = {},
 			}
 
-			-- Ensure the servers and tools above are installed
-			--  To check the current status of installed tools and/or manually install
-			--  other tools, you can run
-			--    :Mason
-			--
-			--  You can press `g?` for help in this menu.
 			require("mason").setup()
 
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+			-- Make sure these binaries are installed
+			--local ensure_installed = {
+			--	"stylua", -- Used to format Lua code
+			--	--"kotlin-lsp",
+			--}
+
+			--require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
 				handlers = {
